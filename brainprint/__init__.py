a5ab10dc9aff882f9fc6ded9adefbf28eec8c83f
AhmedFaisal95 <37597383+AhmedFaisal95@users.noreply.github.com>
2023-03-21 16:10:05 +0100

    Add _version.py

--- brainprint/__init__.py
+++ brainprint/__init__.py
@@ -1,8 +1,2 @@
-# """
-# BrainPrint: A script to compute ShapeDNA of FreeSurfer structures.
-# """
-# __version__ = "0.3.0"
-# from brainprint.brainprint import Brainprint, run_brainprint
-
-__version__ = "0.3.0"
-from .brainprint import Brainprint
+from ._version import __version__  # noqa: F401
+from .brainprint import Brainprint  # noqa: F401
