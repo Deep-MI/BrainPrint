1206f9f4b47d72fbd6f3e330390f3646085c2a19
diersk <kersten.diers@dzne.de>
2024-06-17 18:08:40 +0200

    Removed ventricles, striatum; merged cerebellum cortex and cerebellum white matter

--- brainprint/surfaces.py
+++ brainprint/surfaces.py
@@ -102,25 +102,21 @@ def create_aseg_surfaces(subject_dir: Path, destination: Path) -> dict[str, Path
     # Define aseg labels
 
     # combined and individual aseg labels:
-    # - Left  Striatum: left  Caudate + Putamen + Accumbens
-    # - Right Striatum: right Caudate + Putamen + Accumbens
     # - CorpusCallosum: 5 subregions combined
     # - Cerebellum: brainstem + (left+right) cerebellum WM and GM
-    # - Ventricles: (left+right) lat.vent + inf.lat.vent + choroidplexus + 3rdVent + CSF
+    # - Left-Cerebellum: left cerebellum WM and GM
+    # - Right-Cerebellum: right cerebellum WM and GM
     # - Lateral-Ventricle: lat.vent + inf.lat.vent + choroidplexus
     # - 3rd-Ventricle: 3rd-Ventricle + CSF
 
     aseg_labels = {
         "CorpusCallosum": ["251", "252", "253", "254", "255"],
         "Cerebellum": ["7", "8", "16", "46", "47"],
-        "Ventricles": ["4", "5", "14", "24", "31", "43", "44", "63"],
         "3rd-Ventricle": ["14", "24"],
         "4th-Ventricle": ["15"],
         "Brain-Stem": ["16"],
-        "Left-Striatum": ["11", "12", "26"],
         "Left-Lateral-Ventricle": ["4", "5", "31"],
-        "Left-Cerebellum-White-Matter": ["7"],
-        "Left-Cerebellum-Cortex": ["8"],
+        "Left-Cerebellum": ["7", "8"],
         "Left-Thalamus-Proper": ["10"],
         "Left-Caudate": ["11"],
         "Left-Putamen": ["12"],
@@ -129,10 +125,8 @@ def create_aseg_surfaces(subject_dir: Path, destination: Path) -> dict[str, Path
         "Left-Amygdala": ["18"],
         "Left-Accumbens-area": ["26"],
         "Left-VentralDC": ["28"],
-        "Right-Striatum": ["50", "51", "58"],
         "Right-Lateral-Ventricle": ["43", "44", "63"],
-        "Right-Cerebellum-White-Matter": ["46"],
-        "Right-Cerebellum-Cortex": ["47"],
+        "Right-Cerebellum": ["46", "47"],
         "Right-Thalamus-Proper": ["49"],
         "Right-Caudate": ["50"],
         "Right-Putamen": ["51"],
